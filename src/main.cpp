95b4a851bad15cd01d6331965271877eaa3b7604
Aaron Kison <aaron.kison@gmail.com>
2024-05-05 10:37:47 +0930

    Rebased on project root

---
+++ src/main.cpp
@@ -0,0 +1,44 @@
+#include <Arduino.h>
+#include "Rendering/Renderer.h"
+#include "Rendering/Scene.h"
+#include "Faces/AwuffFace.h"
+#include "ExternalDevices/Displays/HUB75.h"
+#include "ExternalDevices/Inputs/ButtonHandler.h"
+#include "Rendering/Effects/HeartBubbles.h"
+
+auto panel = std::make_unique<HUB75>(100);
+Renderer renderer(std::move(panel));
+
+Scene scene;
+AwuffFace face;
+IEffect* effect;
+ButtonHandler buttonHandler(23);
+
+unsigned long previousMillis;
+
+void setup() {
+    previousMillis = millis();
+
+    renderer.setup();
+
+    scene.addObject(&face);
+}
+
+void loop() {
+    auto time = millis();
+    auto delta = time - previousMillis;
+    previousMillis = time;
+
+    if (buttonHandler.update()) {
+        delete effect;
+        effect = new HeartBubbles(&scene, 20);
+    }
+
+    if (effect) {
+        effect->update(delta);
+    }
+
+    scene.update();
+
+    renderer.render(&scene);
+}
